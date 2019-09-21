74ad717d13c72a1aae699ae99b2f33d1222b9681
sinagaki58 <s.inagaki58@gmail.com>
2019-09-21 20:13:33 +0900

    add function parser

--- monkey/ast/ast.go
+++ monkey/ast/ast.go
@@ -2,6 +2,7 @@ package ast
 
 import (
 	"bytes"
+	"strings"
 
 	"github.com/sinagaki58/monkey-interpreter/monkey/token"
 )
@@ -172,7 +173,7 @@ type IfExpression struct {
 	Alternative *BlockStatement
 }
 
-func (ie *IfExpression) expressionNode()     {}
+func (ie *IfExpression) expressionNode()      {}
 func (ie *IfExpression) TokenLiteral() string { return ie.Token.Literal }
 func (ie *IfExpression) String() string {
 	var out bytes.Buffer
@@ -199,6 +200,31 @@ type Boolean struct {
 func (b *Boolean) expressionNode()      {}
 func (b *Boolean) TokenLiteral() string { return b.Token.Literal }
 func (b *Boolean) String() string       { return b.Token.Literal }
+
+// FunctionLiteral is struct
+type FunctionLiteral struct {
+	Token      token.Token
+	Parameters []*Identifier
+	Body       *BlockStatement
+}
+
+func (fl *FunctionLiteral) expressionNode()      {}
+func (fl *FunctionLiteral) TokenLiteral() string { return fl.Token.Literal }
+func (fl *FunctionLiteral) String() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range fl.Parameters {
+		params = append(params, p.String())
+		out.WriteString(fl.TokenLiteral())
+		out.WriteString("(")
+		out.WriteString(strings.Join(params, ","))
+		out.WriteString(")")
+		out.WriteString(fl.Body.String())
+	}
+
+	return out.String()
+}
 
 // Program is struct
 type Program struct {
