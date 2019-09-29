eddc2a422ec9fd3fdd8701a4c3476f50f94c09fd
sinagaki58 <s.inagaki58@gmail.com>
2019-09-29 11:04:46 +0900

    expand repl

--- monkey/repl/repl.go
+++ monkey/repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/sinagaki58/monkey-interpreter/monkey/evaluator"
 	"github.com/sinagaki58/monkey-interpreter/monkey/lexer"
 	"github.com/sinagaki58/monkey-interpreter/monkey/parser"
 )
@@ -30,8 +31,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
